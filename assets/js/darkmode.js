3741d4625647e189ff304e3b3d7d2873ac2d3d58
Cody S <cody.scoles@gmail.com>
2022-02-22 14:46:05 -0500

    Push toggleMode function

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -6,11 +6,12 @@
 
 
 //A boolean value that determines the state of the mode
-var mode = 'lightmode'
+var mode = 'lightmode';
 
 //References for document elements affected by the color
 //scheme change.
 var body = $('body');
+var header = $('header');
 var navItems = $('header nav ul li');
 var anchorTags = $('a');
 var heading = $('h1');
@@ -21,16 +22,27 @@ var aboutMeSection = $('p.about-me');
 var projectLinks = $('a.project');
 var contactItems = $('div.contact-item, a.contact-item');
 
+var modeElements = [body, navItems, anchorTags, heading, heroSubheading, asideSectionLabels,
+                    subheadings, aboutMeSection, projectLinks, contactItems, header];
+
 
 
 
 //Changes mode
 function toggleMode(){
-
+    debugger;
     switch (mode){
         case 'lightmode':
+            modeElements.forEach(jQueryReference => {
+                jQueryReference.addClass('darkmode');
+            });
+            mode = 'darkmode';
             break;
         case 'darkmode':
+            modeElements.forEach(jQueryReference => {
+                jQueryReference.removeClass('darkmode');
+            });
+            mode = 'lightmode'
             break;
         default: 
             mode = 'lightmode';
@@ -43,5 +55,5 @@ function toggleMode(){
 
 //Gets current mode
 function getMode(){
-
+    console.log("Portfolio is in " + mode + ".");
 };
