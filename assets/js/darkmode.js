5a06ca6f4d64b56cd17d1e78aa20f10a481e8b58
Cody S <cody.scoles@gmail.com>
2022-02-22 13:52:04 -0500

    Link darkmode script and add JQuery refs

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -4,13 +4,39 @@
     if (!mode) return;
   } catch (e) {} })();
 
+
+//A boolean value that determines the state of the mode
+var mode = 'lightmode'
+
 //References for document elements affected by the color
 //scheme change.
+var body = $('body');
+var navItems = $('header nav ul li');
+var anchorTags = $('a');
+var heading = $('h1');
+var heroSubheading = $('#hero-subheading');
+var asideSectionLabels = $('aside.section-label');
+var subheadings = $('h2');
+var aboutMeSection = $('p.about-me');
+var projectLinks = $('a.project');
+var contactItems = $('div.contact-item, a.contact-item');
+
 
 
 
 //Changes mode
 function toggleMode(){
+
+    switch (mode){
+        case 'lightmode':
+            break;
+        case 'darkmode':
+            break;
+        default: 
+            mode = 'lightmode';
+            break;
+    }
+
 
 };
 
