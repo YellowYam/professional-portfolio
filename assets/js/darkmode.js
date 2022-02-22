b5033d8a21b45a777dde3551317680d9548d5536
Cody S <cody.scoles@gmail.com>
2022-02-22 13:20:21 -0500

    Plan darkmode.js and add JQuery to project

---
+++ assets/js/darkmode.js
@@ -0,0 +1,21 @@
+// On document load, get the mode from local storage
+(function() { try {
+    var mode = localStorage.getItem('lightDarkMode');
+    if (!mode) return;
+  } catch (e) {} })();
+
+//References for document elements affected by the color
+//scheme change.
+
+
+
+//Changes mode
+function toggleMode(){
+
+};
+
+
+//Gets current mode
+function getMode(){
+
+};
